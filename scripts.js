414d906f9d214d94b8ebb59aacfd2dfe3ff9c046
MarcosGuilherme <49126645+MarcosGuilherm@users.noreply.github.com>
2020-10-29 20:25:44 -0300

    Atualização do dados

--- scripts.js
+++ scripts.js
@@ -2,17 +2,16 @@ const modalOverlay = document.querySelector('.modal_overlay')
 const cards = document.querySelectorAll('.card')
 
 for(let card of cards){
-  const videoId = card.getAttribute("id");
   card.addEventListener("click",function(){
     modalOverlay.classList.add('active')
-    modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`
+   
   })
 }
 
 
 document.querySelector(".close-modal").addEventListener('click',function(){
   modalOverlay.classList.remove('active')
-  modalOverlay.querySelector('iframe').src =""
+  
 })
 
 
